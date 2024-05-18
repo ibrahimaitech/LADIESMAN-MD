3685e86fe8e79da4d8d73829eb36de04d477b70f
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-05-18 16:17:42 +0300

    LADIESMAN MD

--- commandes/test.js
+++ commandes/test.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "repo", reaction: "👍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗪𝗛𝗔𝗧"𝗦 𝗨𝗣 𝗠𝗬 𝗚𝙀𝙀 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛  \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛 𝗥𝗘𝗣𝗢.*";
-    let d = ' https://github.com/ibrahimaitech/IBRAHIM-AI-10.10/tree/main';
+    let z = '𝗪𝗛𝗔𝗧"𝗦 𝗨𝗣 𝗠𝗬 𝗚𝙀𝙀 𝗧𝗛𝗜𝗦 𝗜𝗦 LADIESMAN-MD  \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *LADIESMAN-MD 𝗥𝗘𝗣𝗢.*";
+    let d = ' https://github.com/ibrahimaitech/LADIESMAN-MD';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/c66d12099fb7a4f62d70a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
